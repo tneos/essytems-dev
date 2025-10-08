7c93963eae50e362e9be07dc054e0db1903a6a14
Thomas Neos <tneos@hotmail.com>
2025-10-08 17:20:42 +0100

    Initialize clerk middleware and update Readme file

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,20 @@
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
-const inter = Inter({ subsets: ['latin'] });
+import type {Metadata} from "next";
+import {Inter} from "next/font/google";
+import "./globals.css";
+import {ClerkProvider} from "@clerk/nextjs";
+const inter = Inter({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: 'Jobify Dev',
-  description: 'Job application tracking system for job hunters',
+  title: "ESSystems Dev",
+  description: "Hospitality business solution",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
-    <html lang='en' suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   );
 }
