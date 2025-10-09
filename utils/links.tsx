54f79bd92d6f449a731e8e70c14768b6005c36dc
Thomas Neos <tneos@hotmail.com>
2025-10-09 07:37:46 +0100

    Update links

---
+++ utils/links.tsx
@@ -0,0 +1,25 @@
+import {AreaChart, Layers, AppWindow} from "lucide-react";
+
+type NavLink = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+};
+
+const links: NavLink[] = [
+  {
+    href: "/add-employee",
+    label: "add job",
+    icon: <Layers />,
+  },
+  {
+    href: "/employees",
+    label: "employees",
+    icon: <AppWindow />,
+  },
+  {
+    href: "/info",
+    label: "info",
+    icon: <AreaChart />,
+  },
+];
