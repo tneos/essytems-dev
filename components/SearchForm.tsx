810e34d951103102c3bb7b6fed493ecaf77984c0
Thomas Neos <tneos@hotmail.com>
2025-10-20 21:45:42 +0100

    Add SearchForm chadcn components and get data from form

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,4 +1,42 @@
+"use client";
+import {Input} from "./ui/input";
+import {usePathname, useRouter, useSearchParams} from "next/navigation";
+import {Button} from "./ui/button";
+
+import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "./ui/select";
+import {JobStatus} from "@/utils/types";
+
 function SearchForm() {
-  return <h1 className="text-4xl">Search Form</h1>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const search = formData.get("search") as string;
+    const jobStatus = formData.get("jobStatus") as string;
+    console.log(search, jobStatus);
+  };
+  return (
+    <form
+      className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
+      onSubmit={handleSubmit}
+    >
+      <Input type="text" placeholder="Search employees" name="search" />
+      <Select name="jobStatus">
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {["all", ...Object.values(JobStatus)].map(jobStatus => {
+            return (
+              <SelectItem key={jobStatus} value={jobStatus}>
+                {jobStatus}
+              </SelectItem>
+            );
+          })}
+        </SelectContent>
+      </Select>
+
+      <Button type="submit">Search</Button>
+    </form>
+  );
 }
 export default SearchForm;
