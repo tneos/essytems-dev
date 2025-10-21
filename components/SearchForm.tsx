f5262c877b5fefbc237df8b90f2b1ece4bbc67ec
Thomas Neos <tneos@hotmail.com>
2025-10-21 17:12:06 +0100

    Get params from search form and set default values

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -7,20 +7,31 @@ import {Select, SelectContent, SelectItem, SelectTrigger, SelectValue} from "./u
 import {JobStatus} from "@/utils/types";
 
 function SearchForm() {
+  const searchParams = useSearchParams();
+  // Get params and set default values
+  const search = searchParams.get("search") || "";
+  const jobStatus = searchParams.get("jobStatus") || "all";
+  const router = useRouter();
+  const pathname = usePathname();
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
     const search = formData.get("search") as string;
     const jobStatus = formData.get("jobStatus") as string;
-    console.log(search, jobStatus);
+    let params = new URLSearchParams();
+    params.set("search", search);
+    params.set("jobStatus", jobStatus);
+
+    router.push(`${pathname}?${params.toString()}`);
   };
   return (
     <form
       className="bg-muted mb-16 p-8 grid sm:grid-cols-2 md:grid-cols-3 gap-4 rounded-lg"
       onSubmit={handleSubmit}
     >
-      <Input type="text" placeholder="Search employees" name="search" />
-      <Select name="jobStatus">
+      <Input type="text" placeholder="Search employees" name="search" defaultValue={search} />
+      <Select name="jobStatus" defaultValue={jobStatus}>
         <SelectTrigger>
           <SelectValue />
         </SelectTrigger>
