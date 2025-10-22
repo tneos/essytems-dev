0559372a35e7082fc02dc63c61b1a1ffd3cb5a65
Thomas Neos <tneos@hotmail.com>
2025-10-22 10:03:16 +0100

    Add EmployeeCard initial setup using Card from shadcn

--- components/EmployeeCard.tsx
+++ components/EmployeeCard.tsx
@@ -1,6 +1,30 @@
 import {EmployeeType} from "@/utils/types";
+import {MapPin, Briefcase, CalendarDays, RadioTower} from "lucide-react";
+
+import Link from "next/link";
+import {Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "./ui/card";
+import {Separator} from "./ui/separator";
+import {Button} from "./ui/button";
+import {Badge} from "./ui/badge";
+import EmployeeInfo from "./EmployeeInfo";
+import DeleteEmployeeBtn from "./DeleteEmployeeBtn";
 
 function EmployeeCard({employee}: {employee: EmployeeType}) {
-  return <h1 className="text-4xl">Employee Card</h1>;
+  return (
+    <Card className="bg-muted">
+      <CardHeader>
+        <CardTitle>{employee.position}</CardTitle>
+        <CardDescription>{employee.fullName}</CardDescription>
+      </CardHeader>
+      <Separator />
+      <CardContent></CardContent>
+      <CardFooter className="flex gap-4">
+        <Button asChild size="sm">
+          <Link href={`/employees/${employee.id}`}>edit</Link>
+        </Button>
+        <DeleteEmployeeBtn />
+      </CardFooter>
+    </Card>
+  );
 }
 export default EmployeeCard;
