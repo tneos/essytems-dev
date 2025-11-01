ff487348bde39d769f3a95005f036d9f26d5d7f8
Thomas Neos <tneos@hotmail.com>
2025-11-01 15:44:06 +0000

    Add setup for ChartsContainer

--- components/ChartsContainer.tsx
+++ components/ChartsContainer.tsx
@@ -1,4 +1,29 @@
+"use client";
+import {BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer} from "recharts";
+import {useQuery} from "@tanstack/react-query";
+import {getChartsDataAction} from "@/utils/actions";
+
 function ChartsContainer() {
-  return <h2 className="text-xl">Charts Container</h2>;
+  const {data} = useQuery({
+    queryKey: ["charts"],
+    queryFn: () => getChartsDataAction(),
+  });
+
+  if (!data || data.length < 1) return null;
+
+  return (
+    <section className="mt-16">
+      <h1 className="text-4xl">Employee additions per month</h1>
+      <ResponsiveContainer width="100%" height={300}>
+        <BarChart data={data} margin={{top: 50}}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" />
+          <YAxis allowDecimals={false} />
+          <Tooltip />
+          <Bar dataKey="count" fill="#009999" barSize="75" />
+        </BarChart>
+      </ResponsiveContainer>
+    </section>
+  );
 }
 export default ChartsContainer;
